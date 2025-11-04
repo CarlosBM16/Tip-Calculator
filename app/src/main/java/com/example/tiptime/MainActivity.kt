cc750f192b9e4f17f706e2d09b4ad3f558299210
Carlos Bermúdez Mancebo <carlosbermudezmancebo@gmail.com>
2025-11-04 18:46:10 +0100

    More tests added and passed

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -187,6 +187,7 @@ fun RoundTheTipRow(
  */
 @VisibleForTesting
 internal fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+    if (amount <= 0) return NumberFormat.getCurrencyInstance().format(0.0)
     var tip = tipPercent / 100 * amount
     if (roundUp) {
         tip = kotlin.math.ceil(tip)
--- app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -14,4 +14,34 @@ class TipCalculatorTests {
         val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
         assertEquals(expectedTip, actualTip)
     }
+
+    @Test
+    fun calculateTip_20PercentRoundup() {
+        val amount = 10.00
+        val tipPercent = 20.00
+
+        var expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, true)
+        assertEquals(expectedTip, actualTip)
+    }
+
+    @Test
+    fun calculateNegativeAmountNoRoundup() {
+        val amount = -10.00
+        val tipPercent = 20.00
+
+        var expectedTip = NumberFormat.getCurrencyInstance().format(0)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
+    }
+
+    @Test
+    fun calculateNegativeAmountRoundup() {
+        val amount = -10.00
+        val tipPercent = 20.00
+
+        var expectedTip = NumberFormat.getCurrencyInstance().format(0)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, true)
+        assertEquals(expectedTip, actualTip)
+    }
 }
